a180eb94399b5be70fb6019ceac702ee5656b101
Nero <jesalemalo@gmail.com>
2023-10-10 14:56:33 -0400

    I did make migrations for Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from authentication.models import MyUser
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    title = models.CharField("Titulo", max_length=50, unique=True)
+    description = models.CharField("Descripcion", max_length=50, unique=True)
+    content = models.TextField()
+
+
+    def __str__(self):
+        return f"{self.title}"
+    
