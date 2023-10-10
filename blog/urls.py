ab0cd0c1924028ffb3ab857afece37788b644568
Nero <jesalemalo@gmail.com>
2023-10-10 18:36:28 -0400

    I fix CRUDBLOG, not return

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import POSTBlogView
+from .views import POSTBlogView, GETBlogView, PUTBlogView, DELETEBlogView
 
 
 urlpatterns = [
     path('create-blog/', POSTBlogView.as_view(), name='create-blog'),
+    path('list-blogs/',GETBlogView.as_view(), name='list-blogs'),
+    path('update-blog/<int:pk>/',PUTBlogView.as_view(), name='update-blogs'),
+    path('delete-blog/<int:pk>/',DELETEBlogView.as_view(), name='delete-blogs'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Blog
-from .serializers import BlogSerializerPOST
+from .serializers import BlogSerializerPOST, BlogSerializerGET
+from authentication.models import MyUser
+
 
 # Create your views here.
+
+class GETBlogView(APIView):
+        
+    def get(self, request, format = None):
+        blog = Blog.objects.filter(author=request.user.id)
+        blog_serializer = BlogSerializerGET(blog, many = True)
+        return Response(blog_serializer.data, status=status.HTTP_200_OK)
+
 
 class POSTBlogView(APIView):
 
@@ -28,3 +39,61 @@ class POSTBlogView(APIView):
             return Response({"message":"Blog creado correctamente"}, status=status.HTTP_201_CREATED)
         else:
             return Response({"error":"Los datos ingresados son incorrectos"}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+class PUTBlogView(APIView):
+
+    def get_object(self, pk):
+        try:
+            blog = Blog.objects.get(pk=pk)
+            return blog
+        except Blog.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        if blog.author == request.user:
+            blog_serializer = BlogSerializerPOST(blog)
+            return Response(blog_serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "No tienes permiso para editar este blog"}, status=status.HTTP_403_FORBIDDEN)
+
+    def put(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        if blog.author == request.user:
+            blog_serializer = BlogSerializerPOST(blog, data=request.data, partial=True)
+            if blog_serializer.is_valid():
+                blog_serializer.save()
+                return Response(blog_serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Los datos ingresados no son válidos"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"error": "No tienes permiso para editar este blog"}, status=status.HTTP_403_FORBIDDEN)
+
+    def handle_exception(self, exc):
+        return Response({"error": str(exc)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+class DELETEBlogView(APIView):
+    def get_object(self, pk):
+        try:
+            blog = Blog.objects.get(pk=pk)
+            return blog
+        except Blog.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        if blog.author == request.user:
+            blog_serializer = BlogSerializerPOST(blog)
+            return Response(blog_serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "No tienes permiso para editar este blog"}, status=status.HTTP_403_FORBIDDEN)
+
+    def delete(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        if blog.author == request.user:
+            blog.delete()
+            return Response({"message": "El blog ha sido eliminado correctamente"}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"error": "No tienes permiso para eliminar este blog"}, status=status.HTTP_403_FORBIDDEN)
